71282f5d9e301a99396bc218af37bf44b7739a66
Ronniel <piscoster@gmail.com>
2018-04-12 05:11:01 +0000

    load css only on rig builder page

--- wp-content/themes/rehub-blankchild/functions.php
+++ wp-content/themes/rehub-blankchild/functions.php
@@ -3,11 +3,13 @@
 add_action('wp_enqueue_scripts', 'enqueue_parent_theme_style');
 function enqueue_parent_theme_style()
 {
-    //css
-    wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
-    wp_enqueue_style('bootstrap-4.0.0', get_stylesheet_directory_uri() . '/css/bootstrap.min.css');
-    wp_enqueue_style('dataTables-1.10.16', get_stylesheet_directory_uri() . '/css/dataTables.bootstrap4.min.css');
-   
-    //js
-    wp_enqueue_script('main-mining-rig-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
+    if ( is_page( 'Rig Builder' ) ) {
+        //css
+        wp_enqueue_style('parent-style', get_template_directory_uri() . '/style.css');
+        wp_enqueue_style('bootstrap-4.0.0', get_stylesheet_directory_uri() . '/css/bootstrap.min.css');
+        wp_enqueue_style('dataTables-1.10.16', get_stylesheet_directory_uri() . '/css/dataTables.bootstrap4.min.css');
+       
+        //js
+        wp_enqueue_script('main-mining-rig-js', get_theme_file_uri('/js/scripts-bundled.js'), NULL, '1.0', true);
+    }
 }
