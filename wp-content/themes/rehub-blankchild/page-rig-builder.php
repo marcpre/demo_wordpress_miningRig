b60605b477c0e62dae77865670b2d8275e8fd318
marcpre <marcus.presich@gmail.com>
2018-04-13 21:57:32 +0200

    add table to modal

--- wp-content/themes/rehub-blankchild/page-rig-builder.php
+++ wp-content/themes/rehub-blankchild/page-rig-builder.php
@@ -20,103 +20,122 @@
                 </div>
                 <article class="post" id="page-<?php the_ID();?>">
                     <?php if (have_posts()): while (have_posts()): the_post();?>
-		                    <?php the_content();?>
-		                    <?php wp_link_pages(array('before' => '<div class="page-link">' . __('Pages:', 'rehub_framework'), 'after' => '</div>'));?>
-		                    <?php endwhile;endif;?>
- 
+                    <?php the_content();?>
+                    <?php wp_link_pages(array('before' => '<div class="page-link">' . __('Pages:', 'rehub_framework'), 'after' => '</div>'));?>
+                    <?php endwhile;endif;?>
+
                     <?php
 $products = new WP_Query(array(
-    'posts_per_page'   => -1,
-    'post_type'        => 'post',
-    //'meta_key'		=> '_cegg_data_Amazon',
-	// 'meta_value'	=> 'Melbourne'
+    'posts_per_page' => -1,
+    'post_type' => 'post',
+    //'meta_key'        => '_cegg_data_Amazon',
+    // 'meta_value'    => 'Melbourne'
 ));
 
 // var_dump($products->posts);
 
-if( $products->have_posts() ) { ?>
+if ($products->have_posts()) {?>
 
- <!-- Button trigger modal -->
- <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
-  Launch demo modal
-</button>
+                        <!-- Button trigger modal -->
+                        <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModal">
+                            Launch demo modal
+                        </button>
 
 
-	<ul>
-	<?php while ( $products->have_posts() ) { $products->the_post()  ?>
-		<li>
-			<a href="<?php the_permalink(); ?>"><?php the_title(); ?></a>
-            </br>
-            <?php 
-            $amazon = get_post_meta( get_the_ID(), '_cegg_data_Amazon', true); 
                         
-            $keys = array_keys($amazon); // convert associative arrays to index array
-            print_r($amazon[$keys[0]]['price']);    
-            print_r($amazon[$keys[0]]['currency']);            
-            print_r($amazon[$keys[0]]['manufacturer']);    
-            print_r($amazon[$keys[1]][0]);  //get title here
-            
-//            print_r($amazon);
-            $var_str = var_export($amazon[$keys[0]], true);
-            file_put_contents('filename.txt', $var_str);
-            ?>
-		</li>
-	<?php }; ?>
-	</ul>
-<?php }; 
-
+                        <?php };
 
 wp_reset_postdata();
-
 ?>
 
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-lg" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <div class="modal-body">
+                        <!-- Modal -->
+                        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+                            <div class="modal-dialog modal-lg" role="document">
+                                <div class="modal-content">
+                                    <div class="modal-header">
+                                        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+                                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                        </button>
+                                    </div>
+                                    <div class="modal-body">
 
-        <table id="table_id" class="display" style="width:100%">
-            <thead>
-                <tr>
-                    <th>#</th>
-                    <th>Title</th>
-                    <th>Description</th>
-                    <th>Price</th>
-                </tr>
-            </thead>
-            <tbody>
-                <tr>
-                    <td>Row 1 Data 1</td>
-                    <td>Row 1 Data 2</td>
-                    <td>Row 1 Data 1</td>
-                    <td>Row 1 Data 2</td>
-                </tr>
-                <tr>
-                    <td>Row 2 Data 1</td>
-                    <td>Row 2 Data 2</td>
-                    <td>lolonator</td>
-                    <td>Row 1 Data 2</td>
-                </tr>
-            </tbody>
-        </table>
+                                                                <table id="table_id" class="display" style="width:100%">
+                            <thead>
+                                <tr>
+                                    <th>#</th>
+                                    <th>Title</th>
+                                    <th>Manufacturer</th>
+                                    <th>Price</th>
+                                    <th>Availability</th>
+                                    <th></th>
+                                    <th></th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <?php 
+                                $i = 0;
+                                while ($products->have_posts()) {
+                                    $products->the_post()?>
 
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
-    </div>
-  </div>
-</div>
+                                </br>
+                                <?php
+$amazon = get_post_meta(get_the_ID(), '_cegg_data_Amazon', true);
+$keys = array_keys($amazon); // convert associative arrays to index array
+    ?>
+                                    <tr>
+                                        <td>
+                                            <?php echo $i++; ?>
+                                        </td>
+                                        <td>
+                                            <img src="<?php echo $amazon[$keys[0]]['img']; ?>" alt="<?php the_title();?>" height="42" width="42">
+                                            <a href="<?php the_permalink();?>">
+                                                <?php the_title();?>
+                                            </a>
+                                        </td>
+                                        <td>
+                                            <?php echo $amazon[$keys[0]]['manufacturer']; ?>
+                                        </td>
+                                        <td>
+                                            <?php
+                                                echo $amazon[$keys[0]]['currency'];
+                                                echo $amazon[$keys[0]]['price'];
+                                            ?>
+                                        </td>
+                                        <td>
+                                            <?php print_r($amazon[$keys[0]]['extra']['availability']);?>
+                                        </td>
+                                        <td>Buy</td>
+                                        <td>Add</td>
+                                    </tr>
 
-                    <!-- END -->
+                                    <?php
+
+    //remove this php part
+    //            print_r($amazon[$keys[0]]['price']);
+    //            print_r($amazon[$keys[0]]['currency']);
+    //            print_r($amazon[$keys[0]]['manufacturer']);
+    //            print_r($amazon[$keys[0]]['img']);
+    //            print_r($amazon[$keys[1]][0]);  //get title here
+
+//            print_r($amazon);
+    //            $var_str = var_export($amazon[$keys[0]], true);
+    //            file_put_contents('filename.txt', $var_str);
+    ?>
+        <?php };?>
+                            </tbody>
+                        </table>
+
+                                    </div>
+                                    <div class="modal-footer">
+                                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                        <button type="button" class="btn btn-primary">Save changes</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+
+                        <!-- END -->
                 </article>
             </div>
 
