dd6ce9564d811b408ffc9da6bb3c7ba80fbb4d79
marcpre <marcus.presich@gmail.com>
2018-04-21 09:22:58 +0200

    refactor css

--- wp-content/themes/rehub-blankchild/functions.php
+++ wp-content/themes/rehub-blankchild/functions.php
@@ -4,12 +4,12 @@ require get_theme_file_path('/inc/computerHardwareRoute.php');
 
 function enqueue_parent_theme_style()
 {
-    if ( is_page( 'Rig Builder' ) ) {
+    if ( is_page( 'rig-builder' ) ) {
         
         $parentStyle = 'parent-style';
         
         //css
-        wp_enqueue_style($parentStyle, get_template_directory_uri() . '/style.css');
+        wp_enqueue_style($parentStyle, get_template_directory_uri() . '/css/style.css');
         wp_enqueue_style('bootstrap-4.0.0', get_stylesheet_directory_uri() . '/css/bootstrap.min.css', array($parentStyle));
         wp_enqueue_style('dataTables', '//cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css', array($parentStyle) );
         wp_enqueue_style('dataTables-1.10.16', get_stylesheet_directory_uri() . '/css/dataTables.bootstrap4.min.css', array($parentStyle));
@@ -24,4 +24,4 @@ function enqueue_parent_theme_style()
     }
 }
 
-add_action('wp_enqueue_scripts', 'enqueue_parent_theme_style', 1);
+// add_action('wp_enqueue_scripts', 'enqueue_parent_theme_style');
