3a9e085665cfa705f7e7a38a661041f52f10d737
marcpre <marcus.presich@gmail.com>
2018-04-24 04:49:47 +0000

    fixing css issue

--- wp-content/themes/rehub-blankchild/functions.php
+++ wp-content/themes/rehub-blankchild/functions.php
@@ -1,16 +1,32 @@
 <?php
 
+// Do not allow directly accessing this file.
+if ( ! defined( 'ABSPATH' ) ) {
+	exit( 'Direct script access denied.' );
+}
+
+if ( defined( 'RH_GRANDCHILD_DIR' ) ) {
+	include( RH_GRANDCHILD_DIR . 'rh-grandchild-func.php' );
+}
+
 require get_theme_file_path('/inc/computerHardwareRoute.php');
 require get_theme_file_path('/inc/miningRigsRoute.php');
 
+
+add_action('wp_enqueue_scripts', 'enqueue_parent_theme_style');
 function enqueue_parent_theme_style()
 {
+    if ( !defined( 'RH_MAIN_THEME_VERSION' ) ) {
+		define('RH_MAIN_THEME_VERSION', '7.2.1');
+	}
+
+    $parentStyle = 'parent-style';
+
+    wp_enqueue_style($parentStyle, get_template_directory_uri() . '/style.css');
+
     if ( is_page( 'Rig Builder' )  || is_page('Mining Rigs')) {
         
-        $parentStyle = 'parent-style';
-        
         //css
-        wp_enqueue_style($parentStyle, get_template_directory_uri() . '/style.css');
         wp_enqueue_style('bootstrap-4.0.0', get_stylesheet_directory_uri() . '/css/bootstrap.min.css', array($parentStyle));
         wp_enqueue_style('dataTables', '//cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css', array($parentStyle) );
         wp_enqueue_style('dataTables-1.10.16', get_stylesheet_directory_uri() . '/css/dataTables.bootstrap4.min.css', array($parentStyle));
@@ -26,4 +42,10 @@ function enqueue_parent_theme_style()
     }
 }
 
-add_action('wp_enqueue_scripts', 'enqueue_parent_theme_style');
+//////////////////////////////////////////////////////////////////
+// Translation
+//////////////////////////////////////////////////////////////////
+add_action('after_setup_theme', 'rehubchild_lang_setup');
+function rehubchild_lang_setup(){
+    load_child_theme_textdomain('rehubchild', get_stylesheet_directory() . '/lang');
+}
