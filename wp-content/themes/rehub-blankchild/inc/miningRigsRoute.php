1228a79f1796b02c248b3fcc5a6cd75c8dc8646a
marcpre <marcus.presich@gmail.com>
2018-04-26 22:01:51 +0200

    create function for array merge oegg_data_Amazon

--- wp-content/themes/rehub-blankchild/inc/miningRigsRoute.php
+++ wp-content/themes/rehub-blankchild/inc/miningRigsRoute.php
@@ -18,28 +18,55 @@ function createMiningRig($data)
 {
     
     //santitize array input
-    $rig = json_encode(array_map('esc_attr', $data['miningRigPostIds']));
+    $rigHardwareArray = json_encode(array_map('esc_attr', $data['miningRigPostIds']));
 
+    
+    setCustomFieldContentEgg();
+    
+    
     //if ($data['miningRig']->count() > 0) {
     return wp_insert_post(array(
         'post_type' => 'Mining-Rig',
         'post_status' => 'publish',
         'post_title' => $data['title'],
         'meta_input' => array(
-            'miningRig' => $rig,
+            'miningRig' => $rigHardwareArray,
         ),
     ));
     
-    // return setCustomFieldContentEgg($postId, $data);
+    // return setCustomFieldContentEgg($miningRigsId, $data);
 }
-/*
-function setCustomFieldContentEgg($postId, $data) 
+
+function setCustomFieldContentEgg()//$miningRigsId, $rigHardwareArray) 
 {
-    echo("<script>console.log('PHP: ".$data."');</script>");
+    $miningRigsId = 100; //Mining Rig Post
+    $rigHardwareArray = ["71","59","71","59"];
     
-    update_post_meta( $postId, '_cegg_data_Amazon', $meta_value ); 
+    // $cfContentEgg = get_post_meta($miningRigsId, '_cegg_data_Amazon', true);
+
+    $results = array();
+        
+    foreach ($rigHardwareArray as $computerHardwareId) {
+        $id = intval($computerHardwareId);
+        
+        $mainQuery = new WP_Query(array(
+            'posts_per_page' => -1,
+            'post_type' => 'Computer-Hardware',
+            'p' => $id,
+        ));
+        
+        $cfContentEgg = get_post_meta($id, '_cegg_data_Amazon', true);
+
+        $results  = array_merge($results, $cfContentEgg);
+        
+        //merge object
+        // $merged = (object) array_merge((array) $merged, (array) $cfContentEgg);
+
+    }
+    
+    update_post_meta( $miningRigsId, '_cegg_data_Amazon', $results ); 
 }
-*/
+
 function allMiningRigs()
 {
     $miningRigsQuery = new WP_Query(array(
