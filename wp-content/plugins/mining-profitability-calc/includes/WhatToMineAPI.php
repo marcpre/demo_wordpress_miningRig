2224fbb232c5636c6a295727b1eed07150d30b67
marcpre <marcus.presich@gmail.com>
2018-05-26 22:31:53 +0200

    creating what to mine table

--- wp-content/plugins/mining-profitability-calc/includes/WhatToMineAPI.php
+++ wp-content/plugins/mining-profitability-calc/includes/WhatToMineAPI.php
@@ -12,6 +12,15 @@ class WhatToMineAPI {
         add_action( 'update_whatToMine_api', 'updateWhatToMineAPI');        
     }
     
+    public function updateWhatToMineAPI() {
+        $whatToMineURL = "http://whattomine.com/coins.json";
+
+        $client = new GuzzleHttp\Client();
+        
+        $response = $client->request('GET', $whatToMineURL)->getBody();
+        $obj = json_decode($response);
+    }
+    
     public function setupCronJob($scheduleTime) {
         //Use wp_next_scheduled to check if the event is already scheduled
         $timestamp = wp_next_scheduled( 'update_whatToMine_api' );
@@ -21,15 +30,6 @@ class WhatToMineAPI {
             //Schedule the event for right now, then to repeat twicedaily using the hook 'update_whatToMine_api'
             wp_schedule_event( time(), $scheduleTime, 'update_whatToMine_api' );
         }
-    }
-    
-    public function updateWhatToMineAPI() {
-        $whatToMineURL = "http://whattomine.com/coins.json";
-
-        $client = new GuzzleHttp\Client();
-        
-        $response = $client->request('GET', $whatToMineURL);
-            
     }
 }
 new WhatToMineAPI();
--- wp-content/plugins/mining-profitability-calc/mining-profitability-calc.php
+++ wp-content/plugins/mining-profitability-calc/mining-profitability-calc.php
@@ -19,9 +19,9 @@ require_once 'vendor/autoload.php';
  */
 class Mining_Profitability_Calculator {
 
-     /**
-     * Constructor.
-     */
+    /**
+    * Constructor.
+    */
     public function __construct() {
         // MinProfCalc constants.
         define( 'MinProfCalc_FILE', __FILE__ );
@@ -30,11 +30,14 @@ class Mining_Profitability_Calculator {
         register_activation_hook( basename( MinProfCalc_DIR ) . '/' . basename( MinProfCalc_FILE ), array( $this, 'activate' ) );
         // add_action( 'init', array( $this, 'load_plugin_textdomain' ) );
         add_action( 'plugins_loaded', array( $this, 'includes' ) );
-		add_action( 'init', array( $this, 'maybe_update' ) );
+        add_action( 'init', array( $this, 'maybe_update' ) );
+        //cron jobs
+        add_action( 'update_whatToMine_api', 'updateWhatToMineAPI');        
+
     }
     /**
-	 * Called on plugin activation
-	 */
+	* Called on plugin activation
+	*/
 	public function activate() {
         $this->includes();
         $this->runCronJobs();
@@ -43,31 +46,32 @@ class Mining_Profitability_Calculator {
         flush_rewrite_rules();
     }
     /**
-     * Includes.
-     */
+    * Includes.
+    */
     public function includes() {
         include_once( MinProfCalc_DIR . 'includes/WhatToMineAPI.php' );
     }
     /**
-     * Cron Jobs.
-     */
+    * Cron Jobs.
+    */
     public function runCronJobs() {
-        WhatToMineAPI::setupCronJob('twicedaily');
+        // WhatToMineAPI::setupCronJob('twicedaily');
+        WhatToMineAPI::updateWhatToMineAPI();
     }
-	/**
-	 * Create tables.
-	 */
+    /**
+	* Create tables.
+	*/
 	public function create_tables() {
 		global $wpdb;
 		$wpdb->hide_errors();
 		require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
         // TODO create new table
-        // dbDelta( $this->get_schema() );
+        dbDelta( $this->get_schema() );
 	}
-	/**
-	 * Get table schema.
-	 * @return string
-	 */
+    /**
+	* Get table schema.
+	* @return string
+	*/
 	private static function get_schema() {
 		global $wpdb;
 		if ( $wpdb->has_cap( 'collation' ) ) {
@@ -76,8 +80,31 @@ class Mining_Profitability_Calculator {
 			$collate = '';
 		}
 		$tables = "
-CREATE TABLE {$wpdb->prefix}mining_prof_calc_history (
+CREATE TABLE {$wpdb->prefix}whatToMine_API (
 id bigint(20) NOT NULL AUTO_INCREMENT,
+id_WhatToMine bigint(20) NOT NULL,
+tag longtext NOT NULL,
+algorithm longtext NOT NULL,
+block_time longtext NOT NULL,
+block_reward longtext NOT NULL,
+block_reward24 DECIMAL NOT NULL,
+last_block DECIMAL NOT NULL,
+difficulty DECIMAL NOT NULL,
+difficulty24 DECIMAL NOT NULL,
+nethash DECIMAL NOT NULL,
+exchange_rate DECIMAL NOT NULL,
+exchange_rate24 DECIMAL NOT NULL,
+exchange_rate_vol DECIMAL NOT NULL,
+exchange_rate_curr longtext NOT NULL,
+market_cap longtext NOT NULL,
+estimated_rewards longtext NOT NULL,
+estimated_rewards24 longtext NOT NULL,
+btc_revenue longtext NOT NULL,
+btc_revenue24 longtext NOT NULL,
+profitability DECIMAL NOT NULL,
+profitability24 DECIMAL NOT NULL,
+lagging BOOLEAN NOT NULL, 
+timestamp datetime NOT NULL,
 PRIMARY KEY  (id)
 ) $collate;
 		";
