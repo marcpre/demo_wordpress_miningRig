ef126e9ba0ff2afea89d654e40e192ff21f573d9
marcpre <marcus.presich@gmail.com>
2018-05-29 05:02:18 +0000

    creating api

--- wp-content/themes/rehub-blankchild/inc/miningProfRoute.php
+++ wp-content/themes/rehub-blankchild/inc/miningProfRoute.php
@@ -37,9 +37,32 @@ function allMiningProfitability()
                                 
     foreach ($mainQuery as $key => $value) {
         array_push($results['miningProfitability'], array(
-            'coin' => $value->coin
+            'id' => '',
+            'coin' => $value->coin,
+            'tag' => $value->tag,
+            'algorithm' => $value->algorithm,
+            'block_time' => $value->block_time,
+            'block_reward' => $value->block_reward,
+            'block_reward24' => floatval($value->block_reward24),
+            'last_block' => floatval($value->last_block),
+            'difficulty' => floatval($value->difficulty),
+            'difficulty24' => $value->difficulty24,
+            'nethash' => floatval($value->nethash),
+            'exchange_rate' => floatval($value->exchange_rate),
+            'exchange_rate24' => floatval($value->exchange_rate24),
+            'exchange_rate_vol' => floatval($value->exchange_rate_vol),
+            'exchange_rate_curr' => $value->exchange_rate_curr,
+            'market_cap' => $value->market_cap,
+            'estimated_rewards' => $value->estimated_rewards,
+            'estimated_rewards24' => $value->estimated_rewards24,
+            'btc_revenue' => $value->btc_revenue,
+            'btc_revenue24' => $value->btc_revenue24,
+            'profitability' => floatval($value->profitability),
+            'profitability24' => floatval($value->profitability24),
+            'timestamp' => date('Y-m-d H:i:s', $value->timestamp),     
         ));
     }
     
     return $results;
+    //return $mainQuery;
 }
