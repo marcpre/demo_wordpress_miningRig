92b65380316875e6089f6fa33e83bf3cf2ac461f
marcpre <marcus.presich@gmail.com>
2018-06-18 09:45:11 +0200

    fix profitability

--- wp-content/plugins/mining-profitability-calc/includes/CalculateProfitability.php
+++ wp-content/plugins/mining-profitability-calc/includes/CalculateProfitability.php
@@ -76,44 +76,57 @@ class CalculateProfitability {
                 WHERE c.symbol = \"" . sanitize_text_field(get_field('tag', $postId)[0]) . "\"
                 ORDER BY c.created_at ASC
                 LIMIT 1;");
-            
-            var_dump($whatToMineRes);
-            var_dump($coinValueRes);
-            
+                       
             /**
              * Calculate Profitability
              */
+            // get variables
             $selectedCoinPriceInUSD = floatval($coinValueRes[0]->price);
-            $hashRate = get_field('hash_rate', $postId)[0];
+            $hashRate = floatval(get_field('hash_rate', $postId));
 
             $networkHashRate = floatval($whatToMineRes[0]->nethash);
             $numberOfEquipment = 1;
             $blockTime = floatval($whatToMineRes[0]->block_time);
             $blockReward = floatval($whatToMineRes[0]->block_reward);
+            $wattofGPUs = get_field('watt_estimate', $postId)[0];
+            $energyCosts = 0.1;
             
             // calculate revenue
             $userRatio = ($hashRate * $numberOfEquipment) / $networkHashRate;
             $blocksPerMinute = 60 / $blockTime;
             $rewardPerMinute = $blocksPerMinute * $blockReward;
  
-            $revenuePerDay = $userRatio * $rewardPerMinute * 60 * 24 * $selectedCoinPriceInUSD;
+            $revenuePerDayInUSD = $userRatio * $rewardPerMinute * 60 * 24 * $selectedCoinPriceInUSD;
             
             // calculate costs
-            $wattofGPUs = get_field('watt', $postId)[0];
-            $energyCosts = 0.1;
+            if($energyCosts === "" || $energyCosts === null || isset($energyCosts)) $energyCosts = 0.1;
             
-            if($energyCosts === "" || $energyCosts === null) $energyCosts = 0.1;
-            
-            $powerCostsPerHour = (($wattofGPUs * $numberOfEquipment) / 1000) * $energyCosts;
-            $powerCostsPerDay = $powerCostsPerHour * 24;
+            $powerCostsPerHourInUSD = (($wattofGPUs * $numberOfEquipment) / 1000) * $energyCosts;
+            $powerCostsPerDayInUSD = $powerCostsPerHourInUSD * 24;
             
             // final results
-            $earningsPerDay = $revenuePerDay - $powerCostsPerDay;
+            $earningsPerDayInUSD = $revenuePerDayInUSD - $powerCostsPerDayInUSD;
             
             /**
              * Insert into db
              */
-            // TODO  
+            $res = array();
+            $res = array(
+                'post_id' => $postId,
+                'coin_id' => $coinValueRes[0]->id,
+                'whatToMine_id' => $whatToMineRes[0]->id,
+                'daily_netProfit' => number_format(floatval($earningsPerDayInUSD), 12),
+                'daily_grossProfit' => number_format(floatval($revenuePerDayInUSD), 12),
+                'daily_costs' => number_format(floatval($powerCostsPerDayInUSD), 12),
+            );
+            
+            try {
+                $res['created_at'] = date('Y-m-d H:i:s');
+                $res['updated_at'] = date('Y-m-d H:i:s');
+                $wpdb->insert("{$wpdb->prefix}miningProfitability", $res);
+            } catch (\Exception $ex) {
+                // ...  
+            }
         }
     }
 }
